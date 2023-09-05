3155259fda2e7d7f5626309dd611f9929223a721
omolayole <omolayoleogunyemi@gmail.com>
2023-09-05 21:29:24 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+
+import yaml
+from yaml.loader import SafeLoader
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
+print('Hash_password', hashed_passwords)
+
+st.markdown(
+    f'<style>{open("assets/style.css").read()}</style>',
+    unsafe_allow_html=True
+)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+# authenticating the user
+if authentication_status:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
+
